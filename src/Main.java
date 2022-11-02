9da553ff86c7b6b42554388140d2742d58a86aa8
Oleg <zkv_testpost@mail.ru>
2022-11-02 19:43:00 +0300

    Miles

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+       int amount = 7_500;
+       int bonus = 20;
+       System.out.println("Бонусных миль:" + amount / bonus);
+
+    }
+}
